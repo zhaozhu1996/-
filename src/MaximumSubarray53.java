c1c037b87d302a015b1b48801099bfcfd47036ba
feng <linfengluo@gmail.com>
2020-02-27 12:06:02 +0800

    子序列最大累加值

---
+++ src/MaximumSubarray53.java
@@ -0,0 +1,21 @@
+public class MaximumSubarray53 {
+    public static int maxSubArray(int[] nums) {
+
+        int ans = nums[0];
+        int sum = 0;
+        for(int num: nums) {
+            if(sum > 0) {
+                sum += num;
+            } else {
+                sum = num;
+            }
+            ans = Math.max(ans, sum);
+        }
+        return ans;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2,1,-3,4,-1,2,1,-5,4}));
+    }
+}
